d78f56347ca445222b6ea2ce695de24fb64ce9d9
Ahmed Abdelmageed <ahmedabdelmageed18@gmail.com>
2024-07-07 04:56:36 +0300

    fix OurTeamSection

--- components/OurTeamSection.tsx
+++ components/OurTeamSection.tsx
@@ -34,7 +34,15 @@ const OurTeamSection = () => {
   );
 };
 
-const TeamCard = ({ image, name, position }) => {
+const TeamCard = ({
+  image,
+  name,
+  position,
+}: {
+  image: string;
+  name: string;
+  position: string;
+}) => {
   return (
     <div className="flex flex-col items-center justify-center gap-4">
       <Image
