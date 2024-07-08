66406e5df7ccd92bd9db594dc032ba9be1133f4f
Ahmed Abdelmageed <ahmedabdelmageed18@gmail.com>
2024-07-08 23:01:10 +0300

    Fix team slider

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { TeamCard } from "@/components/OurTeamSection";
+import { TeamSlider } from "@/components/teams";
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -13,28 +14,7 @@ const Team = () => {
           </h2>
 
           <div>
-            <Swiper spaceBetween={50}>
-              <SwiperSlide>
-                <div className="grid grid-cols-3 gap-5 ">
-                  <TeamCard
-                    image="https://svscoin.org/wp-content/uploads/2024/07/WhatsApp-Image-2024-06-29-at-12.28.50-AM-ct-person.jpeg"
-                    name="Nader said"
-                    position={"CEO"}
-                  />
-
-                  <TeamCard
-                    image="https://svscoin.org/wp-content/uploads/2024/07/WhatsApp-Image-2024-06-28-at-9.27.54-PM-ct-person.jpeg"
-                    name="Mary Pelgone"
-                    position="Developer"
-                  />
-                  <TeamCard
-                    name="Yazan Abbasi"
-                    position="CTO"
-                    image="https://svscoin.org/wp-content/uploads/2024/07/WhatsApp-Image-2024-06-28-at-9.27.51-PM-ct-person.jpeg"
-                  />
-                </div>
-              </SwiperSlide>
-            </Swiper>
+            <TeamSlider />
           </div>
         </div>
       </div>
