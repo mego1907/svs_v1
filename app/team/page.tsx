e16881d71ceadf8145681dec1682b4fe7d37b425
Ahmed Abdelmageed <ahmedabdelmageed18@gmail.com>
2024-07-09 01:39:46 +0300

    fix font color

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -6,7 +6,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 
 const Team = () => {
   return (
-    <div className="bg-mainBg min-h-screen">
+    <div className="bg-mainBg min-h-screen text-white">
       <div className="pt-40">
         <div className="container mx-auto">
           <h2 className="text-center text-4xl leading-snug font-semibold mb-8">
