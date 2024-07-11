755dd5e2a7df675ef6d29dc05a107525f4468d66
Ahmed Abdelmageed <ahmedabdelmageed18@gmail.com>
2024-07-11 20:37:40 +0300

    Improve SEO

--- components/SEO.tsx
+++ components/SEO.tsx
@@ -17,7 +17,6 @@ const SEO: React.FC = () => {
       <link rel="icon" href="/assets/images/logoo.png" />
       <meta property="og:title" content="SVS" />
       <meta property="og:description" content="SVS coin" />
-      <meta property="og:image" content={"/assets/images/logo-share.jpg"} />
       <meta property="og:type" content="website" />
       <meta property="twitter:card" content="/assets/images/logo-share.jpg" />
       <meta property="twitter:title" content={"SVS"} />
@@ -26,6 +25,10 @@ const SEO: React.FC = () => {
         property="twitter:image"
         content={"/assets/images/logo-share.jpg"}
       />
+      <meta property="og:image" content="/assets/images/logo-share.jpg" />
+      <meta property="og:image:type" content="jpg" />
+      <meta property="og:image:width" content="300" />
+      <meta property="og:image:height" content="300" />
     </Head>
   );
 };
