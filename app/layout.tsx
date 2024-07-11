14baf2bc870b626195d20eb1a8b78f93134ac01e
Ahmed Abdelmageed <ahmedabdelmageed18@gmail.com>
2024-07-11 21:31:47 +0300

    add meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,30 +12,30 @@ const inter = Inter({ subsets: ["latin"] });
 const montserrat = Montserrat({ subsets: ["latin"] });
 const dm_sans = DM_Sans({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "SVS ",
-  description: "SVS coin",
-  openGraph: {
-    type: "website",
-    url: "https://svs-v1.vercel.app/",
-    title: "SVS ",
-    description: "SVS coin",
-    images: [
-      {
-        url: "https://nextjs.org/og.png", // Must be an absolute URL
-        width: 800,
-        height: 600,
-      },
-      {
-        url: "https://nextjs.org/og-alt.png", // Must be an absolute URL
-        width: 1800,
-        height: 1600,
-        alt: "My custom alt",
-      },
-    ],
-  },
-  icons: ["/assets/images/logo.png", "/assets/images/logo-share.jpg"],
-};
+// export const metadata: Metadata = {
+//   title: "SVS ",
+//   description: "SVS coin",
+//   openGraph: {
+//     type: "website",
+//     url: "https://svs-v1.vercel.app/",
+//     title: "SVS ",
+//     description: "SVS coin",
+//     images: [
+//       {
+//         url: "https://nextjs.org/og.png", // Must be an absolute URL
+//         width: 800,
+//         height: 600,
+//       },
+//       {
+//         url: "https://nextjs.org/og-alt.png", // Must be an absolute URL
+//         width: 1800,
+//         height: 1600,
+//         alt: "My custom alt",
+//       },
+//     ],
+//   },
+//   icons: ["/assets/images/logo.png", "/assets/images/logo-share.jpg"],
+// };
 
 export default function RootLayout({
   children,
@@ -45,6 +45,12 @@ export default function RootLayout({
   return (
     <PrimeReactProvider>
       <html lang="en">
+        <head>
+          <link rel="icon" href="/assets/images/logo-share.jpg" />
+          <link rel="apple-touch-icon" href="/assets/images/logoo.png" />
+          <meta name="description" content="SVS coin" />
+          <title>SVS</title>
+        </head>
         <body className={dm_sans.className}>
           <ParticlesBackrgound />
 
