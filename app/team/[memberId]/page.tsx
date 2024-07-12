54d3b60d2dd196911af939426a138ac6c382260b
Ahmed Abdelmageed <ahmedabdelmageed18@gmail.com>
2024-07-12 09:20:19 +0300

    update SEO in team member

--- app/team/[memberId]/page.tsx
+++ app/team/[memberId]/page.tsx
@@ -11,23 +11,31 @@ import { FaFacebookF, FaXTwitter } from "react-icons/fa6";
 export async function generateMetadata(props): Promise<Metadata> {
   console.log("Generat metadata props :", props);
 
-  const firstMember = teamData[0];
+  const member = teamData.find((member) => member.id === props.params.memberId);
 
   return {
-    title: firstMember.name,
+    title: member?.name,
     description: "member in SVS coin",
     keywords: ["SVS", "coin", "blockchain"],
     openGraph: {
-      url: `https://svs-v1.vercel.app/team/${firstMember.id}/`,
-      title: firstMember.name,
-      description: firstMember.desc,
+      url: `https://svs-v1.vercel.app/team/${member?.id}/`,
+      title: member?.name,
+      description: member?.desc,
       images: ["https://svs-v1.vercel.app/assets/images/logo-share.jpg"],
     },
   };
 }
 
-const TeamMember = () => {
-  const { memberId } = useParams();
+const TeamMember = ({
+  params: { memberID },
+  searchParams,
+}: {
+  params: { memberID: string };
+  searchParams: {};
+}) => {
+  // const { memberId } = useParams();
+
+  const member = teamData.find((member) => member.id === +memberID);
 
   // const selectedMember = useMemo(
   //   () => ,
@@ -50,10 +58,7 @@ const TeamMember = () => {
               <div className="flex items-center justify-center">
                 <div className="md:w-[400px] w-[200px] h-[200px] md:h-[400px] rounded-3xl z-40 relative overflow-hidden md:px-0 px-6">
                   <Image
-                    src={
-                      teamData.find((member) => member.id === +memberId)
-                        ?.image || ""
-                    }
+                    src={member?.image || ""}
                     fill
                     objectFit="cover"
                     alt=""
@@ -62,45 +67,32 @@ const TeamMember = () => {
                 </div>
               </div>
 
-              <h3 className="text-2xl font-bold">
-                {teamData.find((member) => member.id === +memberId)?.name}
-              </h3>
+              <h3 className="text-2xl font-bold">{member?.name}</h3>
               <div className="flex gap-4">
                 <a
-                  href={
-                    teamData.find((member) => member.id === +memberId)
-                      ?.socialMedia.facebook
-                  }
+                  href={member?.socialMedia.facebook}
                   className="w-10 h-10 rounded-full bg-gray-300 flex justify-center items-center"
                 >
                   <FaFacebookF color="#111827" fontSize={18} />
                 </a>
                 <a
-                  href={
-                    teamData.find((member) => member.id === +memberId)
-                      ?.socialMedia.linkedin
-                  }
+                  href={member?.socialMedia.linkedin}
                   className="w-10 h-10 rounded-full bg-gray-300 flex justify-center items-center"
                 >
                   <BsLinkedin color="#111827" fontSize={18} />
                 </a>
                 <a
-                  href={
-                    teamData.find((member) => member.id === +memberId)
-                      ?.socialMedia.twitter
-                  }
+                  href={member?.socialMedia.twitter}
                   className="w-10 h-10 rounded-full bg-gray-300 flex justify-center items-center"
                 >
                   <FaXTwitter color="#111827" fontSize={18} />
                 </a>
               </div>
-              <p className="text-base">
-                {teamData.find((member) => member.id === +memberId)?.position}
-              </p>
+              <p className="text-base">{member?.position}</p>
             </div>
 
             <p className="text-center mt-10 text-gray-300 md:px-60 px-5">
-              {teamData.find((member) => member.id === +memberId)?.desc}
+              {member?.desc}
             </p>
           </div>
         </div>
