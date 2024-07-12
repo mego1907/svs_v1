794b5058c48793ee622eb41747dba0bf3e09c4a6
Ahmed Abdelmageed <ahmedabdelmageed18@gmail.com>
2024-07-12 09:52:18 +0300

    update SEO in team member

--- app/page.tsx
+++ app/page.tsx
@@ -22,17 +22,17 @@ import {
 import SEO from "@/components/SEO";
 import { Metadata } from "next";
 
-export const metaData: Metadata = {
-  title: "SVS ",
-  description: "SVS coin",
-  keywords: ["SVS", "coin", "blockchain"],
-  openGraph: {
-    url: "https://svs-v1.vercel.app",
-    title: "SVS",
-    description: "SVS coin",
-    images: ["https://svs-v1.vercel.app/assets/images/logo-share.jpg"],
-  },
-};
+// export const metaData: Metadata = {
+//   title: "SVS ",
+//   description: "SVS coin",
+//   keywords: ["SVS", "coin", "blockchain"],
+//   openGraph: {
+//     url: "https://svs-v1.vercel.app",
+//     title: "SVS",
+//     description: "SVS coin",
+//     images: ["https://svs-v1.vercel.app/assets/images/logo-share.jpg"],
+//   },
+// };
 
 export default function Home() {
   return (
